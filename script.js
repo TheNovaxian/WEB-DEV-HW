c5b9ee9c9053b7345dc5cb5f4a7653e3aafc811a
TheNovaxian <shaquille-36@hotmail.com>
2024-02-21 09:50:28 -0500

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $('.region').click(function () {
       var region = $(this).data('region');
-      if (region == 'region1') {
+      if (region === 'region1') {
         window.location.href = 'https://javascript.info/';
-      } else if (region == 'region2') {
+      } else if (region === 'region2') {
         window.location.href = 'https://www.css3.info/';
-      } else if (region == 'region3') {
+      } else if (region === 'region3') {
         window.location.href = 'https://html.spec.whatwg.org/multipage/';
       } else {
         window.location.href = 'https://www.collegelasalle.com';
